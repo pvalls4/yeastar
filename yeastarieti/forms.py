a1c814c6b3c9ff55ddcacffc0e1cb9c837c27582
pvalls4 <valls.vilalta.4@gmail.com>
2023-06-12 13:12:36 +0200

    More styling

--- yeastarieti/forms.py
+++ yeastarieti/forms.py
@@ -1,5 +1,7 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm, UserChangeForm, AuthenticationForm
+from django.forms import Widget
+from django.core.validators import MinLengthValidator, MaxLengthValidator
 from .models import *
 # from .models import CustomUser
 
@@ -21,8 +23,8 @@ class LoginForm(AuthenticationForm):
 # myfield = forms.CharField(widget=forms.TextInput(attrs={'class': 'myfieldclass'}))
 
 class MessageForm(forms.ModelForm):
-    text = forms.CharField(widget=forms.TextInput(attrs={'class': 'm-3 p-0.5 text-blue-800 rounded-md placeholder:font-italic', 'placeholder':'Escribe tu SMS aquí...'}))
-    receiver = forms.CharField(widget=forms.TextInput(attrs={'type': 'number', 'max_length':'9','class': 'p-0.5 text-blue-800 rounded-md placeholder:font-italic', 'placeholder':'Escribe el número destino.'}))
+    text = forms.CharField(label='Mensaje', widget=forms.TextInput(attrs={'class': 'm-3 p-0.5 px-1 text-blue-800 rounded-md placeholder:font-italic', 'placeholder':'Escribe tu SMS aquí...'}))
+    receiver = forms.CharField(label='Destinatario', max_length="9", widget=forms.TextInput(attrs={'type': 'number','class': 'p-0.5 px-1 text-blue-800 rounded-md placeholder:font-italic', 'placeholder':'Escribe el número destino.'}))
     
     def clean_my_field(self):
         data = self.cleaned_data['receiver']
@@ -34,10 +36,30 @@ class MessageForm(forms.ModelForm):
         fields = ['text', 'receiver']
 
 class CreateUserForm(UserCreationForm):
-    email = forms.EmailField(max_length=254, help_text='Requerido. Ingrese una dirección de correo válida.')
-    phone = forms.CharField(max_length=9, help_text='Requerido. Ingrese su número de teléfono.')
-    department = forms.CharField(max_length=100, help_text='Requerido. Ingrese su departamento.')
-    max_sms = forms.IntegerField(help_text='Requerido. Ingrese el número máximo de mensajes que puede enviar al día.')
+    email = forms.EmailField(label="E-Mail", max_length=254, widget=forms.EmailInput(attrs={'class': 'm-2 p-0.5 px-1.5 text-blue-800 rounded-md placeholder:font-italic', 'placeholder':'Correo electrónico', 'title': 'Ingrese una dirección de correo válida.'}))
+    phone = forms.IntegerField(label="Teléfono",validators=[MinLengthValidator(9), MaxLengthValidator(9)],  widget=forms.TextInput(attrs={'class': 'm-2 p-0.5 px-1.5 text-blue-800 rounded-md placeholder:font-italic', 'placeholder':'Teléfono', 'title': 'Ingrese su número de teléfono.'}))
+    department = forms.CharField(label="Departamento", max_length=100,  widget=forms.TextInput(attrs={'class': 'm-2 p-0.5 px-1.5 text-blue-800 rounded-md placeholder:font-italic', 'placeholder':'Departamento', 'title': 'Ingrese el departamento al que pertenece.'}))
+    max_sms = forms.IntegerField(label="Número máximo de SMS/día",  widget=forms.TextInput(attrs={'class': 'm-2 p-0.5 px-1.5 text-blue-800 rounded-md placeholder:font-italic', 'placeholder':'SMS máximos', 'title': 'Ingrese el número máximo de mensajes que puede enviar al día.'}))
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['username'].label = 'Usuario'
+        self.fields['username'].widget.attrs['class'] = 'm-2 p-0.5 px-1.5 text-blue-800 rounded-md placeholder:font-italic'
+        self.fields['username'].widget.attrs['placeholder'] = 'Usuario'
+        self.fields['username'].widget.attrs['title'] = 'Ingrese su nombre de usuario.'
+        self.fields['username'].help_text = ''
+
+        self.fields['password1'].label = 'Contraseña'
+        self.fields['password1'].widget.attrs['class'] = 'm-2 p-0.5 px-1.5 text-blue-800 rounded-md placeholder:font-italic'
+        self.fields['password1'].widget.attrs['placeholder'] = 'Contraseña'
+        self.fields['password1'].widget.attrs['title'] = 'Ingrese su contraseña.'
+        self.fields['password1'].help_text = ''
+
+        self.fields['password2'].label = 'Repita su contraseña'
+        self.fields['password2'].widget.attrs['class'] = 'm-2 p-0.5 px-1.5 text-blue-800 rounded-md placeholder:font-italic'
+        self.fields['password2'].widget.attrs['placeholder'] = 'Repita la contraseña'
+        self.fields['password2'].widget.attrs['title'] = 'Ingrese de nuevo su contraseña.'
+        self.fields['password2'].help_text = ''
 
     class Meta:
         model = User
